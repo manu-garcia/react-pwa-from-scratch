c5ea30bbcdf2ccbec7c64aaacabc70cfc4db4a7a
manu-garcia <garcia.martin.manu@gmail.com>
2017-11-25 15:09:53 +0000

    Search prototype

--- src/search/search.js
+++ src/search/search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FaSearch from 'react-icons/lib/fa/search';
 
 import './search.scss';
 
@@ -6,7 +7,64 @@ class Search extends Component {
   render () {
     return (
       <div className="search-component">
-        <p>Searching...</p>
+        <div className="search-header">
+          <input type="text" name="searchKeyword" />
+          <FaSearch className="search-icon"/>
+        </div>
+        <div className="search-content-box">
+          <div className="search-refine-box">
+            <div className="header">Refine search</div>
+            <div>
+              <ul>
+                <li><input type="checkbox" />Profiles</li>
+                <li><input type="checkbox" />Bands</li>
+                <li><input type="checkbox" />Events</li>
+              </ul>
+            </div>
+          </div>
+          <div className="search-results-box">
+          <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+            <div className="search-result-box">
+              <span className="search-result-title">Result example title</span>
+              <p>This is a result exaple description</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
